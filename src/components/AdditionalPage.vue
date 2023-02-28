0c5f3c1dff3b34947170ad06cbd7817c5391e518
thiruparanDev <thiruparanb@gmail.com>
2023-02-28 16:45:06 +0530

    table updated

--- src/components/AdditionalPage.vue
+++ src/components/AdditionalPage.vue
@@ -1,14 +1,62 @@
 <template>
   <div class="additional">
     <h1>This is an additional page</h1>
-    <div></div>
+    <div>{{newObj}}</div>
+    <!-- <div>{{newObjRef}}</div> -->
+    <div>{{newObjRef1}}</div>
+    <button @click="onClickHandler">click</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent,reactive, watch, computed } from 'vue';
 
 export default defineComponent({
+  setup(){
+    interface obj{
+      person:{name:string, Age:number},
+      Animal:{name:string, colour:string},
+      dog?:{name:string}
+    }
+    const newObj:obj = {
+      person: {
+        name: "Thiru",
+        Age: 26
+      },
+      Animal: {
+        name:"dog",
+        colour:"black"
+      }
+    };
+    const newObjRef = reactive({
+      person: {
+        name: "Bala",
+        Age: 72
+      },
+      Animal: {
+        name:"dog",
+        colour:"orange"
+      }
+    });
+    const newObjRef1 = reactive(newObj);
+    watch(newObj,()=>{console.log("newObj changed");});
+    watch(newObjRef,()=>{console.log("newObjRef changed");});
+    // const newOb = computed(()=>{
+    //   return newObj;
+    // });
+
+    const onClickHandler=()=>{
+      newObjRef1.dog={name:'d'};
+      // newObjRef.Animal.colour = "orange";
+    };
+
+    return {
+      newObj,
+      newObjRef,
+      onClickHandler,
+      newObjRef1
+    };
+  }
 });
 </script>
 
